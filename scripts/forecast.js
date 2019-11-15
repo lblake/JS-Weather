1c94a6c2e8591f6985200cae5462d55bf683f95a
Lloyd <lloyd@lloyds-mbp.home>
2019-11-15 10:32:22 +0000

    Added getWeather and getCity functions

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,15 +1,27 @@
-const key = 'TRn6cu0ARrCozL814uhMjMJim7zgNweC';
+const apikey = "TRn6cu0ARrCozL814uhMjMJim7zgNweC";
 
-const getCity = async (city) => {
-  const baseURL =
-    'http://dataservice.accuweather.com/locations/v1/cities/search';
+//Get weather information
+const getWeather = async id => {
+  const baseURL = "http://dataservice.accuweather.com/currentconditions/v1/";
 
-  const query = `?apikey=${key}&q=${city}`;
+  const query = `${id}?apikey=${apikey}`;
 
   const repsonse = await fetch(baseURL + query);
   const data = await repsonse.json();
 
-  console.log(data);
+  return data[0];
+};
+//Get city information
+const getCity = async city => {
+  const baseURL =
+    "http://dataservice.accuweather.com/locations/v1/cities/search";
+
+  const query = `?apikey=${apikey}&q=${city}`;
+
+  const repsonse = await fetch(baseURL + query);
+  const data = await repsonse.json();
+
+  return data[0];
 };
 
-getCity('manchester');
+
